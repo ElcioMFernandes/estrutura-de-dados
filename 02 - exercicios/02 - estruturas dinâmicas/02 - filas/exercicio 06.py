2e305d01cd56f7e0ad79e87c88063c2a46d5a437
Élcio M. Fernandes <elciomateusfernandes1@gmail.com>
2024-05-14 20:18:31 -0300

    Adicionado enunciado comentado nos exercicios 06 e 07 Filas encadeadas.

---
+++ 02 - exercicios/02 - estruturas dinâmicas/02 - filas/exercicio 06.py
@@ -0,0 +1,19 @@
+## Faça um programa de gerenciamento de ordem para atendimento ao cliente.
+## O cliente chegará para ser atendido, receberá uma senha sequencial e aguardará na
+## fila até ser chamado. A senha estará atrelada ao cartão fidelidade do cliente que
+## possui nome e motivo do atendimento. (Gere a senha automaticamente e armazene
+## todos os atributos no mesmo objeto).
+## Ao ser chamado para atendimento, o primeiro cliente da fila é removido.
+## É possível que os atendentes confiram todos os dados referente aos clientes que
+## estão aguardando na fila (seguindo o conceito de fila, apenas o primeiro pode ser
+## manipulado).
+## Ao final do dia de trabalho, é necessário gerar um relatório onde constem todos os
+## atendimento realizados naquele período, com o horário de atendimento de cada
+## cliente (Adicione um atributo ao cliente e manipule uma fila a parte para gerar esse
+## relatório).
+## =====As opções de menu são: ======
+## 1 - Receber cliente na fila.
+## 2 - Chamar para atendimento.
+## 3 - Visualizar fila de clientes aguardando.
+## 4 - Visualizar fila de clientes atendidos.
+## Obs: Lembre de indicar o horário de atendimento de cada cliente.
---
+++ 02 - exercicios/02 - estruturas dinâmicas/02 - filas/exercicio 07.py
@@ -0,0 +1,11 @@
+## Para um dado número inteiro n > 1, o menor inteiro d > 1 que
+## divide n é chamado de fator primo. É possível determinar a
+## fatoração prima de n achando-se o fator primo d e substituindo n
+## pelo quociente n / d, repetindo essa operação até que n seja igual
+## a 1.
+## Utilizando o TAD Fila Encadeada para auxiliá-lo na manipulação de
+## dados, implemente uma função que compute a fatoração prima
+## (push) de um número, imprimindo os seus fatores em ordem
+## decrescente (pop).
+## Por exemplo, para n=3960, deverá ser impresso:
+## 11 * 5 * 3 * 3 * 2 * 2 * 2.
