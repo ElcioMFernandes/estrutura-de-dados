d7e9d6f0aa25f3ff46039a6fbb855e4f72947032
Élcio M. Fernandes <elciomateusfernandes1@gmail.com>
2024-05-14 20:33:53 -0300

    Adicionado comentário no exemplo de pilha encadeada e começado o desenvolvimento do exercicio 05 filas encadeadas

--- 02 - exercicios/02 - estruturas dinâmicas/01 - pilhas/exemplo.py
+++ 02 - exercicios/02 - estruturas dinâmicas/01 - pilhas/exemplo.py
@@ -1,51 +1,52 @@
-class Node: ## Claase 
-    def __init__(self):
-        self.__value = 0
-        self.next = None
+class Node: ## Classe para o nó
 
-    def get_value(self):
-        return self.__value
+    def __init__(self) -> None: ## Método construtor do nó
+        self.__value = None ## Valor do nó
+        self.__next = None ## Endereço do próximo nó
 
-    def set_value(self, value):
-        self.__value = value
+    def get_value(self): ## Método para obter o valor do nó
+        return self.__value ## Retorna o valor do nó
 
-    def get_next(self):
-        return self.next
+    def set_value(self, value): ## Método para definir o valor do nó
+        self.__value = value ## Define o valor do nó
 
-    def set_next(self, next):
-        self.next = next
+    def get_next(self): ## Método para obter o endereço do próximo nó
+        return self.__next ## Retorna o endereço do próximo nó
 
-class Stack:
-    def __init__(self):
-        self.__top = None        
+    def set_next(self, __next): ## Método para definir o endereço do próximo nó
+        self.__next = __next ## Define o endereço do próximo nó
 
-    def push(self):
-        temp_node = Node()
-        if temp_node:
-            temp_node.set_value(int(input("value: ")))
-            temp_node.set_next(self.__top)
-            self.__top = temp_node
+class Stack: ## Classe para a pilha
 
-    def pop(self):
-        if self.__top:
-            print("value to remove: ", self.__top.get_value())
-            self.__top = self.__top.get_next()
-        else:
-            print("empty stack...")
-
-    def get_all(self):
-        if not self.__top:
-            print("empty stack...")
-            return
+    def __init__(self): ## Método construtor da pilha
+        self.__top = None ## Topo da pilha
         
-        temp_node = self.__top
-        output = "stack:\n"
-        while temp_node:
-            output += f"{str(temp_node.get_value())}\n"
-            temp_node = temp_node.get_next()
-        print(output)
+    def push(self): ## Método para inserir um novo valor na pilha
+        new = Node() ## Instancia um novo nó
+        if new: ## Se o nó foi instanciado
+            new.set_value(int(input("value: "))) ## Define o valor do nó
+            new.set_next(self.__top) ## O próximo do novo nó é o topo da pilha
+            self.__top = new ## O novo nó é o topo da pilha
 
+    def pop(self): ## Método para remover o valor do topo da pilha
+        if self.__top: ## Se a pilha não estiver vazia
+            print("value to remove: ", self.__top.get_value()) ## Exibe o valor do topo
+            self.__top = self.__top.get_next() ## O topo da pilha é o próximo do topo
+        else: ## Se a pilha estiver vazia
+            print("empty stack...") ## Exibe mensagem de pilha vazia
 
+    def get_all(self): ## Método para exibir todos os valores da pilha
+        if not self.__top: ## Se a pilha estiver vazia
+            print("empty stack...") ## Exibe mensagem de pilha vazia
+            return ## Encerra o método
+        new = self.__top ## Nó temporário
+        output = "stack:\n" ## Saída
+        while new: ## Enquanto houver um nó
+            output += f"{str(new.get_value())}\n" ## Adiciona o valor do nó na saída
+            new = new.get_next() ## O nó temporário recebe o próximo nó
+        print(output) ## Exibe a saída
+
+## Menu
 app = Stack()
 while True:
     op = int(input("\n1. stack up\n2. unstack\n3. get stack\n4. quit\n\nentry: "))
--- 02 - exercicios/02 - estruturas dinâmicas/02 - filas/exercicio 05.py
+++ 02 - exercicios/02 - estruturas dinâmicas/02 - filas/exercicio 05.py
@@ -1,3 +1,23 @@
 ## Faça um programa que cadastre em uma pilha encadeada vários números. A entrada deles será finalizada com a digitação de um número menor ou igual a zero.
 ## Posteriormente, o programa deve gerar duas filas encadeadas, a primeira com os números pares e a segunda com os números ímpares.
 ## A saída do programa deve apresentar a pilha digitada e as filas geradas. Caso alguma das filas seja vazia, deve-se imprimir “Fila vazia”.
+
+class Node:
+    def __init__ (self):
+        self.__value = None
+        self.__next = None
+
+    def set_value(self, value):
+        self.__value = value
+
+    def get_value(self):
+        return self.__value
+    
+    def set_next(self, next):
+        self.__next = next
+
+    def get_next(self):
+        return self.__next
+    
+class Stack:
+    pass
